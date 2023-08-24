a78201d441c56152b89cddea3baaf144c03a1e45
Rohan Survase <102210317+Rohan2604@users.noreply.github.com>
2023-08-24 17:55:06 +0530

    Add files via upload

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExists(int categoryId)
+        {
+            return _context.Categories.Any(c => c.Id == categoryId);
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            //Change Tracking
+            //add, updating, modifying, 
+            //connected or disconnected architecture
+            // EntityState.Added
+            _context.Add(category);
+
+            return Save();
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            _context.Remove(category);
+            return Save();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(c => c.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();   // sql is generated and data is sent in database.
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            _context.Update(category);
+            return Save();
+        }
+    }
+}
